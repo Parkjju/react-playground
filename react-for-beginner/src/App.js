66f5e06d90a32d284a3b6d68ec86d46957aad488
Parkjju <rudwns3927@gmail.com>
2022-03-31 12:28:59 +0900

    Feature: React18 createRoot API

--- react-for-beginner/src/App.js
+++ react-for-beginner/src/App.js
@@ -1,11 +1,27 @@
 import Button from './Button';
 import styles from './App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log('Rendered!');
+  const runOnlyOnce = () => {
+    console.log('Run only once!');
+  };
+  useEffect(runOnlyOnce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={'Click me'} />
+      <input
+        value={keyword}
+        type='text'
+        onChange={onChange}
+        placeholder='Search here...'
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
--- react-for-beginner/src/index.js
+++ react-for-beginner/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import App from './App';
+import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
+
+const container = document.getElementById('root');
+
+const root = ReactDOMClient.createRoot(container);
 
 ReactDOM.render(
   <React.StrictMode>
